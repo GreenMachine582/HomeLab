061e8b75e3433df50bc4470b3a56e190c6fe5e1e
Matthew <greenchicken1902@gmail.com>
2025-06-07 13:05:24 +1000

    refactor(grafana): changed to dockerfile usage instead of running a sh script

--- grafana/fetch_dashboards.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir -p /var/lib/grafana/dashboards
-curl -sSL "https://grafana.com/api/dashboards/1860/revisions/36/download" \
-  -o /var/lib/grafana/dashboards/node_exporter_full.json
