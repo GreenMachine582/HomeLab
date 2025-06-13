5f950a5aa09a6362957df049288908e065b2320f
Matthew <greenchicken1902@gmail.com>
2025-06-13 19:17:37 +1000

    refactor(on shutdown): added commands to setup script

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@ export $(cat .env | xargs)
 
 # This script sets up the Alertmanager configuration by substituting environment variables
 envsubst < ./alertmanager/alertmanager.yml.tmpl > ./alertmanager/alertmanager.yml
+
+# Copy the event notification scripts to the appropriate directories
+rm /lib/systemd/system-shutdown/on-shutdown.sh
+xcopy ./on-shutdown.sh /lib/systemd/system-shutdown/on-shutdown.sh
+chmod +x /lib/systemd/system-shutdown/on-shutdown.sh
