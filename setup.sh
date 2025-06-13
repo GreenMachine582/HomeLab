01a2db1e262a1a8465d329bb82623861ab4261e0
Matthew <greenchicken1902@gmail.com>
2025-06-13 19:28:14 +1000

    fix(on shutdown): xcopy is a windows command + can't remove file if it doesn't exist

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ envsubst < ./alertmanager/alertmanager.yml.tmpl > ./alertmanager/alertmanager.ym
 # Copy the event notification scripts to the appropriate directories
 DST_ON_SHUTDOWN="/lib/systemd/system-shutdown/on-shutdown.sh"
 if [ -f "$DST_ON_SHUTDOWN" ]; then
-  rm "$DST"
+  rm "$DST_ON_SHUTDOWN"
 fi
-copy ./on-shutdown.sh "$DST"
-chmod +x "$DST"
+copy ./on-shutdown.sh "$DST_ON_SHUTDOWN"
+chmod +x "$DST_ON_SHUTDOWN"
